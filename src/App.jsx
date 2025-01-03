e458e2271e586ee729e9d48b9d4219030cb3cf94
iamrohandhiman <iamrohandhiman@gmail.com>
2025-01-04 00:18:36 +0530

    added map

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import { AreaSelection } from './pages/AreaSelection'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from './components/common/Navbar';
-
+import MapComponent from './components/specific/MapComponent.jsx';
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar/>
-      <Routes>
+      <Routes> 
+      <Route path="/user/select/area/map" element={  <MapComponent/>}/>
       <Route path="/user/select/area" element={<AreaSelection />}/>
      </Routes>
     </BrowserRouter>
--- src/pages/AreaSelection.jsx
+++ src/pages/AreaSelection.jsx
@@ -3,12 +3,13 @@ import { Navbar } from '../components/common/Navbar';
 import Spline from '@splinetool/react-spline';
 import { ProgressBar } from '../components/specific/ProgressBar';
 import { getWeather } from '../services/weatherCall';
+import MapComponent from '../components/specific/MapComponent';
 
 export const AreaSelection = () => {
   const [weatherData, setWeatherData] = useState(23); // Default to null
-
-    
-
+  const [selectedPointsParent,setSelectedPointsParent] = useState([])
+  const [pointLocationParent,setPointsLocationParent] = useState([])
+  const [polygonAreaParent,setPolygonAreaParent] = useState(0)
   return (
     <>
       <div className="absolute w-full">
@@ -24,11 +25,11 @@ export const AreaSelection = () => {
       </div>
 
       <div className="absolute mt-[294px] w-full bg-blue-900 h-screen">
-        <div className="w-full flex justify-center items-center">
+        <div className="w-full flex justify-between items-center">
           {/* Left */}
-          <div className="mt-8">
+          <div className="mt-8 ml-8">
             {/* Upper Div */}
-            <div className="flex justify-start items-center px-11 w-[641px] h-[123px] bg-blue-950 border-2 border-gray-500 rounded-[29px]">
+            <div className="flex justify-start items-center px-11 w-[641px] h-[123px] bg-blue-950 border-2 border-gray-500 rounded-[29px] mb-5">
               {/* Select Area */}
               <div className="w-[135px] text-[30px] text-white">Select Area</div>
 
@@ -42,19 +43,42 @@ export const AreaSelection = () => {
             </div>
 
             {/* Lower Div Info Upload */}
-            <div className="flex justify-start items-center px-11 w-[641px] h-[546px] bg-blue-950 border-2 border-gray-500 rounded-[29px]">
+            <div className=" px-11 w-[641px] h-[546px] bg-blue-950 border-2 border-gray-500 rounded-[29px]">
               {weatherData ? (
-                <div className="text-black text-xl w-[80%] text-[13px] font-semi font-serif bg-orange-300 border-[1px] border-orange-500">your selected location is a <span className='font-bold'>prime spot </span>weather is  <span className='font-bold'>{weatherData}°C</span></div>
+                <div className= "mt-5 text-black text-xl w-[80%] text-[16px] font-semi font-serif bg-orange-300 border-[1px] border-orange-500">your selected location is a <span className='font-bold'>prime spot </span>weather is  <span className='font-bold'>{weatherData}°C</span></div>
               ) : (
-                <div className="text-black text-xl w-[80%] text-[13px] font-mono bg-orange-300 border-[1px] border-orange-500">Loading weather data...</div>
+                <div className="text-black text-xl w-[80%] text-[13px] font-mono bg-orange-300 border-[1px] border-orange-500 p-5 ">Loading weather data...</div>
               )}
-
+              <div className='font-sans text-white text-xl p-5 bg-blue-900  rounded-[13px]'><span className='font-bold'>{"Location → " }</span>{pointLocationParent[0]}</div>
+              <div className='font-sans text-white text-xl p-5 bg-blue-900 rounded-[13px] mt-5'><span className='font-bold'>{"Area → " }</span>{polygonAreaParent} m²</div>
               
+              {/* line seprating search area */}
+
+              <div className='w-[80%] h-[1px] bg-white mt-3 ml-12'></div>
+              <div className='text-[40px] text-white'>Search Area</div>
+              <div className='w-[546px] flex items-center justify-center'>
+                <textarea  className=' flex items-center justify-center w-[546px]' name="" id="" placeholder='Search for Area'></textarea>
+              </div>
+
+
+              {/* test logging for selected poonts */}
+              {console.log(selectedPointsParent)}
+              {console.log(pointLocationParent)}
+              {console.log(polygonAreaParent)}
             </div>
           </div>
 
           {/* Right */}
-          <div>Map</div>
+          <div>
+            <div className='mt-[25px] p-5 rounded-[25px] bg-blue-950  mr-4'>
+            <MapComponent 
+            setSelectedPointsParent={setSelectedPointsParent} 
+            setPointsLocationParent={setPointsLocationParent}
+            setPolygonAreaParent={setPolygonAreaParent}
+            />
+            
+            </div>
+          </div>
         </div>
       </div>
     </>
